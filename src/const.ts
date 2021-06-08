389934ce12033200f5d9761737dae20dd758df33
oskar <ooskar@gmx.de>
2021-06-09 00:16:58 +0200

    added const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,1 +1,9 @@
 export const ITEMS_FILE_PATH = __dirname + '/assets/Items.json';
+export const ACCOUNT_FILE_PATH = __dirname + '/assets/Account.json';
+
+// IPC MESSAGES
+export const IPC_ITEMS_GET_ALL = 'items:getAll';
+export const IPC_ITEMS_UPDATE = 'items:update';
+export const IPC_ITEM_UPGRADE = 'item:upgrade';
+export const IPC_MONEY_GET = 'money:get';
+export const IPC_MONEY_UPDATE = 'money:update';
