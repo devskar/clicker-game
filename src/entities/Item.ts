a49aac8a4b915b1c38d31fc49973a4ef3e15afb0
oskar <ooskar@gmx.de>
2021-06-10 19:11:34 +0200

    updated Item.json and Item.ts

--- src/entities/Item.ts
+++ src/entities/Item.ts
@@ -1,11 +1,14 @@
-interface Item {
+interface Items {
   name: string;
   id: number;
   price: number;
+  priceMultiplier: number;
+  moneyPerClick: number;
+  moneyPerClickMultiplier: number;
   moneyPerSecond: number;
-  multiplier: number;
+  moneyPerSecondMultiplier: number;
   currentLevel: number;
   description: string;
 }
 
-export default Item;
+export default Items;
