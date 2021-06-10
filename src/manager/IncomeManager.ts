36c0f4801153eef03e28b766f36da338a67c9ba6
oskar <ooskar@gmx.de>
2021-06-10 20:09:23 +0200

    ?

---
+++ src/manager/IncomeManager.ts
@@ -0,0 +1,40 @@
+import { round } from '../utils';
+import ItemManager from './ItemManager';
+
+class IncomeManager {
+  itemManager: ItemManager;
+
+  constructor(ItemManager: ItemManager) {
+    this.itemManager = ItemManager;
+  }
+
+  getMoneyPerSecond() {
+    let moneyPerSecond: number = 0;
+
+    for (const item of this.itemManager.getItems()) {
+      if (item.currentLevel > 0) {
+        moneyPerSecond +=
+          item.moneyPerSecond *
+          (1 + item.currentLevel * item.moneyPerSecondMultiplier);
+      }
+    }
+
+    return round(moneyPerSecond);
+  }
+
+  getMoneyPerClick() {
+    let moneyPerClick: number = 0;
+
+    for (const item of this.itemManager.getItems()) {
+      if (item.currentLevel > 0) {
+        moneyPerClick +=
+          item.moneyPerClick *
+          (1 + item.currentLevel * item.moneyPerClickMultiplier);
+      }
+    }
+
+    return round(moneyPerClick);
+  }
+}
+
+export default IncomeManager;
