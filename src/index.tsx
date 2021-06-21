dba19bdc33c0a953eaf86305ce52ebc942f908dc
oskar <ooskar@gmx.de>
2021-06-21 12:46:51 +0200

    renamed animations.css -> effects.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
-import './styles/animations.css';
+import './styles/effects.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
