ece17443c9ed174c57fe4c3f9627b91761c33b43
oskar <ooskar@gmx.de>
2021-08-22 13:33:00 +0200

    small update

--- src/components/SettingsWindow.tsx
+++ src/components/SettingsWindow.tsx
@@ -1,16 +1,6 @@
 import { ipcRenderer } from 'electron';
 import React, { useEffect, useState } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
-import {
-  IPC_LANGUAGE_CHANGE,
-  IPC_SETTINGSWINDOW_OPEN,
-  IPC_USER_BACKGROUNDVOLUME_CHANGE,
-  IPC_SETTINGS_BACKGROUNDVOLUME_GET,
-  IPC_BACKGROUNDVOLUME_REPLY,
-  LANGUAGES_MAP,
-  CSS_HIGHLIGHT_COLOR_VARIABLE,
-  CSS_HIGHLIGHT_EXAMPLE_COLORS,
-} from '../const';
+import { IPC_SETTINGSWINDOW_OPEN } from '../const';
 import HighlightColorPicker from './settings/HighlightColorPicker';
 import LanguageSelector from './settings/LanguageSelector';
 import MusicVolumeInput from './settings/MusicVolumeInput';
@@ -19,7 +9,7 @@ interface Props {}
 
 const SettingsWindow: React.FC<Props> = () => {
   // WINDOW
-  const [shown, setShown] = useState(true);
+  const [shown, setShown] = useState(false);
 
   useEffect(() => {
     ipcRenderer.on(IPC_SETTINGSWINDOW_OPEN, () => {
--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
-import enMessages from '../assets/lang/en.json';
 import deMessages from '../assets/lang/de.json';
+import enMessages from '../assets/lang/en.json';
 
 export const ITEMS_FILE_PATH = __dirname + '/assets/Items.json';
 export const UPGRADES_FILE_PATH = __dirname + '/assets/Upgrades.json';
@@ -30,7 +30,7 @@ export const IPC_USER_BACKGROUNDVOLUME_CHANGE = 'user_backgroundvolume:change';
 export const IPC_BACKGROUNDVOLUME_REPLY = 'backgroundvolume:reply';
 
 export const CSS_HIGHLIGHT_COLOR_VARIABLE = '--highlight-color';
-export const CSS_HIGHLIGHT_EXAMPLE_COLORS = ['blue', 'purple', 'gray'];
+export const CSS_HIGHLIGHT_EXAMPLE_COLORS = ['gold', 'blue', 'purple', 'gray'];
 
 export const LANGUAGES_MAP = {
   en: enMessages,
